d1b055d2cfe762f5b918caa68a20b95469117661
deniz <yanard18@gmail.com>
2026-03-21 05:02:23 +0300

    setup mendatory files

---
+++ README.md
@@ -0,0 +1,3 @@
+## Static Variables
+
+Static variable only initialized once, so they keep the value after function call ends.
---
+++ get_next_line.c
@@ -0,0 +1,12 @@
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+
+}
+
+int	main(void)
+{
+	get_next_line(1);
+	return (0);
+}
